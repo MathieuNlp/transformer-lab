de332b3ef348803a5a2889775f1099e909edbbdf
mnalpon <mnalpon@weborama.com>
2025-07-26 19:52:19 +0200

    attn checkpoint

--- src/gpt.py
+++ src/gpt.py
@@ -1,6 +1,7 @@
 import torch
+import numpy as np
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 class PositionalEncoding(nn.Embedding):
     pass
@@ -18,8 +19,15 @@ class CausalSelfAttention(nn.Module):
     def forward(self, x):
         q = self.wq(x)
         k = self.wk(x)
-        a = 
-        v = 
+        v = self.wv(x)
+
+        attn_weight = q @ k.transpose(-2, -1)
+        scaled_attn_weight = attn_weight / np.sqrt(self.d)
+        scaled_attn_weight = F.softmax(scaled_attn_weight)
+        attn = scaled_attn_weight @ v.transpose(-2, -1)
+
+        return attn
+
 
 
 class Dropout(nn.Module):
