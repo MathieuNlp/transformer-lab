3e42b38c49fa4655ebde758904c23738ee37be27
MatW <mathieunlp@gmail.com>
2025-09-24 09:11:31 +0200

    creating vocabulary

--- src/bpe.py
+++ src/bpe.py
@@ -1,6 +1,7 @@
 from collections import Counter, deque
 from functools import lru_cache
 import json
+from multiprocessing import process
 
 class Tokenizer:
     def __init__(self):
@@ -12,4 +13,33 @@ class Tokenizer:
         self.bpe_merges = {}
         self.bpe_ranks = {}
     
-    
+    def train(self, text: str, vocab_size: int, allowed_special: set={"<|endoftext|>"}):
+        # Preprocess the data -> Replace space with "Ġ"
+        processed_text = []
+        for i, char in enumerate(text):
+            if char == " " and i != 0:
+                processed_text.append("Ġ")
+            if char != " ":
+                processed_text.append(char)
+        processed_text = "".join(processed_text)
+        
+        unique_chars = [chr(i) for i in range(256)]
+        unique_chars.extend(
+            char for char in sorted(set(processed_text))
+            if char not in unique_chars
+        )
+        if "Ġ" not in unique_chars:
+            unique_chars.append("Ġ")
+
+        self.vocab = {i: char for i, char in enumerate(unique_chars)}
+        self.inverse_vocab = {char: i for i, char in self.vocab.items()}
+
+
+
+
+if __name__ == "__main__":
+    tokenizer = Tokenizer()
+    text = "This chapter is about tokenization."
+    VOCAB_SIZE = 52_000
+    tokenizer.train(text, VOCAB_SIZE)
+    print(tokenizer.vocab[196])
